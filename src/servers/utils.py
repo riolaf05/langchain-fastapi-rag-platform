9adf540979f5aa454e895eb6cfdbe04197dc81be
Rosario Laface <lafacerosario@gmail.com>
2024-03-27 12:45:18 +0100

    update

--- src/servers/utils.py
+++ src/servers/utils.py
@@ -73,7 +73,7 @@ class AWSTexttract:
 class AWSTranscribe:
     
         def __init__(self, job_uri, region):
-            self.transcribe = boto3.client('transcribe', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), region=region)
+            self.transcribe = boto3.client('transcribe', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), region_name=region)
             self.job_verification = False
             self.job_uri=job_uri
 
