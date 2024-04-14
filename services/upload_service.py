0292868494db55e1c7e9856eab292d6426d07e07
Rosario Laface <lafacerosario@gmail.com>
2024-04-14 15:20:48 +0200

    updating s3 bucket

--- services/upload_service.py
+++ services/upload_service.py
@@ -1,8 +1,8 @@
-from config.environments import AWS_S3_BUCKET_NAME
+from config.environments import AWS_S3_BUCKET_NAME, AWS_TEXT_S3_BUCKET_NAME
 from config.constants import RAW_DOCUMENT_FOLDER
 from utils.aws_services import AWSS3
 
-s3_client = AWSS3(AWS_S3_BUCKET_NAME)
+s3_client = AWSS3(AWS_TEXT_S3_BUCKET_NAME)
 
 def upload_file_to_s3(file_obj, file_name):
     s3_client.upload_file(
